e612a5b0f7c35668c7dc792bea6113547b1b6fc5
Jeremiah Bolden <Jeremiah791@revature.net>
2022-06-06 12:34:30 -0400

    Reimbursments and User models made

---
+++ src/main/java/com/revature/reimburse/DAOs/UsersDAO.java
@@ -0,0 +1,37 @@
+package com.revature.reimburse.DAOs;
+
+import com.revature.reimburse.models.Users;
+
+import java.util.List;
+
+public class UsersDAO implements CrudDAO<Users>{
+    @Override
+    public void save(Users obj) {
+
+    }
+
+    @Override
+    public void update(Users obj) {
+
+    }
+
+    @Override
+    public void delete(Users obj) {
+
+    }
+
+    @Override
+    public Users getByID(String id) {
+        return null;
+    }
+
+    @Override
+    public Users getByEmail(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Users> getAll() {
+        return null;
+    }
+}
