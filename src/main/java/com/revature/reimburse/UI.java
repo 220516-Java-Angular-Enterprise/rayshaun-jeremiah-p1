e9078822765935ec796114326e99d3aa7c7eae78
MrNoms <tiddle96@gmail.com>
2022-06-09 23:01:01 -0700

    Completed encryption implementation. Please recreate users if necessary

--- src/main/java/com/revature/reimburse/UI.java
+++ src/main/java/com/revature/reimburse/UI.java
@@ -2,10 +2,16 @@ package com.revature.reimburse;
 
 import com.revature.reimburse.util.Security.RSA;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Base64;
+import java.util.LinkedHashMap;
+
 public class UI {
     public static void main(String[] a) {
-        RSA r = new RSA();
-        System.out.printf("Primary Key P(%s,\n\t\t\t  %s)\n",
-                r.e, r.n);
+        try(RSA r = RSA.getKey()) {
+            String sCat = r.encrypt("cat");
+            System.out.println(sCat);
+        } catch(IOException ignore) {}
     }
 }
