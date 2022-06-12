709f8cb3a02cf832c7a84f4e4e75c29b7e769c00
MrNoms <tiddle96@gmail.com>
2022-06-12 11:58:28 -0700

    Initial logging

--- src/main/java/com/revature/reimburse/Servlets/UserServlet.java
+++ src/main/java/com/revature/reimburse/Servlets/UserServlet.java
@@ -3,13 +3,18 @@ package com.revature.reimburse.Servlets;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.reimburse.Services.TokenService;
 import com.revature.reimburse.Services.UserService;
+import com.revature.reimburse.util.FileLogger;
+import com.revature.reimburse.util.Security.RSA;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger logger = FileLogger.getLogger(UserServlet.class.getName());
 
     private final ObjectMapper mapper;
     private final UserService userService;
@@ -22,5 +27,6 @@ public class UserServlet extends HttpServlet {
     }
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+        logger.info("Initiating post request.");
     }
 }
